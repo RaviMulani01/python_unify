5665fbd9e9a2da57e2f0ec3a249c56f5133dfc6f
rootboy1 <ravimulani101@gmail.com>
2023-09-08 11:24:11 -0400

    Update Massage

--- whatsappMessage.py
+++ whatsappMessage.py
@@ -2,7 +2,7 @@ import pywhatkit as kit
 
 # Phone number (including country code) and message
 phone_number = "+1"  # Replace with the recipient's phone number
-message = "Hello"
+message = "HI THERE!"
 
 # Specify the time in 24-hour format (e.g., 16:30 for 4:30 PM)
 hour = 16
